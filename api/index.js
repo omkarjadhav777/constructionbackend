f3f59d0cf5f01824107b102ba9ce0fa727dca9a6
omkar jadhav <oj6875770@gmail.com>
2025-07-12 17:41:19 +0530

    Refactored for serverless support on Vercel

--- api/index.js
+++ api/index.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 const serverless = require('serverless-http');
-
-require('dotenv').config();
-
-const entryRoutes = require('../routes/entries');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/entries', entryRoutes);
-
-// Connect MongoDB (only if not already connected)
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
-  .catch(err => console.log("MongoDB error", err));
-
-app.get('/', (req, res) => {
-  res.send('Backend running on Vercel 🚀');
+app.get('/api', (req, res) => {
+  res.send('🚀 Construction Tracker API is working!');
 });
 
-module.exports = app;
+// Export as serverless function
 module.exports.handler = serverless(app);
